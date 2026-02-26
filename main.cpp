13bf316793b2282bc663e325d5db5cd56ea1707a
Vishnupriya Chunduri <vishnupriyachunduri@Vishnupriyas-MacBook-Air-5.local>
2026-02-26 10:01:05 -0800

    main framework

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <fstream>//for file input
+using namespace std;
 
+const int MAX_SIZE= 100;
+int heap [MAX_SIZE +1]; // heap array,index starts at 1
+int size = 0 // number of elemets in heap
+
+int main()
+  {
+    cout << "Heap program " << endl;
+    return 0;
+
+  }
+
+
+
+
+
+
+
+/*
 void printTree(int array[] int last index, int car index, int depth)
 {if( curIndex*2)+1 <lastIndex){// Assumes index starting at 1,might be off by one w/your last index
   printTree (array,lastIndex, (curIndex*2)+1,depth +1);//right recustion
@@ -10,3 +31,4 @@ for (int i=0 ; i <depth;i++){
 cout << array [curIndex] << endl;
 
 //recursion for left side
+*/
